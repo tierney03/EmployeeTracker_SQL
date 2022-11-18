cc084da6b2e0551a2e2db6f632edebda053c6639
tierney03 <tierney_03@mac.com>
2022-11-18 11:56:20 -0600

    Worked with tutor on schema

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,5 +1,30 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-USE Employees;
+USE employees_db;
 
+CREATE TABLE department (
+    id INT PRIMARY KEY auto_increment,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY auto_increment,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY auto_increment,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role (id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
+    ON DELETE SET NULL
+);
