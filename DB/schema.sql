001ee9df969bbaa0975b764f65867936eb1b1a8f
tierney03 <tierney_03@mac.com>
2022-12-27 12:01:57 -0500

    formatting

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,13 +1,10 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
-
 USE company_db;
-
 CREATE TABLE department (
     id INT PRIMARY KEY auto_increment,
     name VARCHAR(30) NOT NULL
 );
-
 CREATE TABLE roles (
     id INT PRIMARY KEY auto_increment,
     title VARCHAR(30) NOT NULL,
@@ -15,7 +12,6 @@ CREATE TABLE roles (
     department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
-
 CREATE TABLE employee (
     id INT PRIMARY KEY auto_increment,
     first_name VARCHAR(30) NOT NULL,
@@ -23,6 +19,6 @@ CREATE TABLE employee (
     role_id INT NOT NULL,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
-    ON DELETE SET NULL
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE
+    SET NULL
 );
--- DB/seeds.sql
+++ DB/seeds.sql
@@ -1,20 +1,15 @@
 INSERT INTO department(names)
-VALUES
-    ("Engineering"),
+VALUES ("Engineering"),
     ("Sales"),
     ("Finance"),
     ("Legal");
-
 INSERT INTO roles(title, salary, department_id)
-VALUES
-    ("Software Engineer", 120000, 001),
+VALUES ("Software Engineer", 120000, 001),
     ("Account Manager", 160000, 002),
     ("CFO", 200000, 003),
     ("Lawyer", 50000, 004);
-
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES
-    ("Nola", "Allen", 001, 1),
+VALUES ("Nola", "Allen", 001, 1),
     ("Tierney", "Allen", 002, NULL),
     ("Theo", "Allen", 003, 1),
     ("Chris", "Allen", 004, 1);
